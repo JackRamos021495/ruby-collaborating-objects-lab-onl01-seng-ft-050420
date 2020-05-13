5d2b085999ae4341d79d99bdc80cbcb565f3e76c
Jackson Ramos <jackramos021495@gmail.com>
2020-05-13 23:09:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,13 +11,13 @@ attr_accessor :name
     @@all
   end
 
-#  def songs
-#    Song.all.select {|song| song.artist == self}
-#  end
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
 
-#  def add_song(song)
-#    song.artist = self
-#  end
+  def add_song(song)
+    song.artist = self
+  end
 
 
   def self.find_or_create_by_name(name)
