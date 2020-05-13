8cda8fbce21cc5e4a0a950a5d480165b59da8960
Jackson Ramos <jackramos021495@gmail.com>
2020-05-13 23:13:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,6 +11,13 @@ class Song
     @@all
     end
 
+    def self.new_by_filename(file)
+      name = file.gsub(".mp3","").split(" - ")
+      s = Song.new(name[1])
+      s.artist_name = name[0]
+      s
+    end
+    
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
   end
